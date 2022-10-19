a7ea07b82b2213af224ac30fb47c610ba42ca44f
SunnyMohite1993 <sunny.mohite@necsws.com>
2022-10-19 17:22:26 +0530

    New changes

--- testdata/pole15447-pole15453.sql
+++ testdata/pole15447-pole15453.sql
@@ -1,12 +1,1 @@
--- Description: Data model changes #533 Connect v6.11.5 Hellenistic CON-116371.
-
-insert into POLE_UPGRADE_SCRIPT_LOG
-(SCRIPT_NAME, DATA_MODEL_NAME, PROVENANCE, DATE_EXECUTED) values
-('pole15447-pole15453.sql', 'Hellenistic', 'CON-116371', sysdate);
-commit;
-
--- Column creates (excluding make not null):
-alter table "SEARCH_PERSON_INFO" add "OUTCOME_REASON" varchar2(40 char) null;
-alter table "SEARCH_VEHICLE_INFO" add "OUTCOME_REASON" varchar2(40 char) null;
-
--- End
+select SCRIPT_NAME from ( select * from POLE_UPGRADE_SCRIPT_LOG order by SCRIPT_NAME DESC) where rownum=1;
